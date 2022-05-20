95a4cb5b76d981ff9b8cd206dc2aa912923b30c7
Wesley Li <wesleyl@canva.com>
2022-05-20 11:12:26 +1000

    improved

--- report.py
+++ report.py
@@ -49,13 +49,13 @@ def main():
 
     total_requests = 0
     filtered_requests = 0
+    exit_nonzeroes = 0
     cache_hits = 0
     cache_misses = 0
     largest_stdout_len = 0
     longest_duration = 0
     stderr_lens = set()
     stdout_variations = set()
-    exit_nonzeroes = set()
 
     for filename in os.listdir():
         with open(filename, mode="r", encoding="UTF-8") as file:
@@ -63,44 +63,45 @@ def main():
             while (l := file.readline().rstrip()):
                 line = parseLine(l)
 
-                if args.stdin_limit == -1 or line["stdin_len"] <= args.stdin_limit:
-                    total_requests += 1
-                    
-                    if first_line is None:
-                        first_line = line
-                    elif line["start_time"] > first_line["end_time"]:
-                        cache_hits += 1
-                    else:
-                        cache_misses += 1
-
-                    if line["stdout_len"] > largest_stdout_len:
-                        largest_stdout_len = line["stdout_len"]
-
-                    if line["duration"] > longest_duration:
-                        longest_duration = line["duration"]
-
-                    stderr_lens.add(line["stderr_len"])
-
-                    if abs(line["stdout_len"]-first_line["stdout_len"])/first_line["stdout_len"] > 0.01:
-                        stdout_variations.add(filename)
-
-                else:
-                    filtered_requests += 1
-
                 if line["exit_code"] != 0:
-                    exit_nonzeroes.add(filename)
+                    exit_nonzeroes += 1
+                else:
+                    if args.stdin_limit == -1 or line["stdin_len"] <= args.stdin_limit:
+                        total_requests += 1
+
+                        if first_line is None:
+                            first_line = line
+                        elif line["start_time"] > first_line["end_time"]:
+                            cache_hits += 1
+                        else:
+                            cache_misses += 1
+
+                        if line["stdout_len"] > largest_stdout_len:
+                            largest_stdout_len = line["stdout_len"]
+
+                        if line["duration"] > longest_duration:
+                            longest_duration = line["duration"]
+
+                        stderr_lens.add(line["stderr_len"])
+
+                        if abs(line["stdout_len"]-first_line["stdout_len"])/first_line["stdout_len"] > 0.1:
+                            stdout_variations.add(filename)
+
+                    else:
+                        filtered_requests += 1
 
     if args.stdin_limit != -1:
         print(f"stdin_limit {args.stdin_limit}")
         print(f"filtered_requests {filtered_requests} {percent(filtered_requests, total_requests+filtered_requests)}")
 
+    print(f"total_requests {total_requests}")
     print(f"cache_hits {cache_hits} {percent(cache_hits, total_requests)}")
     print(f"cache_misses {cache_misses} {percent(cache_misses, total_requests)}")
+    print(f"exit_nonzeroes {exit_nonzeroes} {percent(exit_nonzeroes, total_requests+filtered_requests+exit_nonzeroes)}")
     print(f"largest_stdout {largest_stdout_len}")
     print(f"longest_duration {longest_duration}")
     print(f"stderr patterns {' '.join(map(str, sorted(stderr_lens)))}")
     print(f"stdout_variations {' '.join(stdout_variations)}")
-    print(f"exit_nonzeroes {' '.join(exit_nonzeroes)}")
 
 
 if __name__ == "__main__":
